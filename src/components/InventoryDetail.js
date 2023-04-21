1b76cdc25341529739b3b2dc99f1c74d0f777444
Eliot Gronstal <eliot.lauren@gmail.com>
2023-04-21 14:35:38 -0700

    WIP: boostrap possibly overriding CSS stylesheet.

--- src/components/InventoryDetail.js
+++ src/components/InventoryDetail.js
@@ -1,7 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 
-function InventoryDetail(props){
+function InventoryDetail(props) {
   const { inventory, onClickingDelete } = props; 
 
   return (
