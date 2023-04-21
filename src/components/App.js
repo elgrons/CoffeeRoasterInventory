fd0af703f31ac667d4d2eb68efdff841faf2db59
Eliot Gronstal <eliot.lauren@gmail.com>
2023-04-21 15:48:29 -0700

    import Bootstrap into App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,10 +9,12 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 function App() {
   return (
     <React.Fragment>
+      <div className="App min-vh-100 d-flex justify-content-center align-items-center">
       <Container>
       <Header />
       <InventoryControl />
       </Container>
+      </div>
     </React.Fragment>
   );
 }
